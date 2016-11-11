c7123898374fa85c0e957efdb408a5aae3e3535f
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2016-11-11 16:07:03 -0300

    saco import que está de más

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { AccountPage } from '../pages/account/account';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
 import { TabsPage } from '../pages/tabs/tabs';
-import { TutorialPage } from '../pages/tutorial/tutorial';
 
 import { ConferenceData } from '../providers/conference-data';
 import { UserData } from '../providers/user-data';
