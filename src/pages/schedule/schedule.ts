144dec685d14e4c75811d177f0f979e7ceadd9a9
Nicolas Dimarco <nicolasdimarco@fiqus.com>
2016-11-23 15:19:50 -0300

    saco import que está de más

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -6,8 +6,6 @@ import { AlertController, App, ItemSliding, List, ModalController, NavController
   To learn how to use third party libs in an
   Ionic app check out our docs here: http://ionicframework.com/docs/v2/resources/third-party-libs/
 */
-import moment from 'moment';
-
 import { ConferenceData } from '../../providers/conference-data';
 import { ScheduleFilterPage } from '../schedule-filter/schedule-filter';
 import { SessionDetailPage } from '../session-detail/session-detail';
